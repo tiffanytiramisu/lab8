76b183b2fb582ab2200e9a2f913add9a937b9981
Tiffany Su <ttsu@ucsd.edu>
2018-02-28 22:55:39 -0800

    thirdpartyAPI lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	var ucsd_ltlng = {lat:32.878839, lng:-117.235901};
+ 	var map = new google.maps.Map(document.getElementById('map'), {
+ 		center: ucsd_ltlng,
+ 		zoom: 17
+ 	});
+
+ 	var marker = new google.maps.Marker({
+ 		position:ucsd_ltlng,
+ 		map:map,
+ 		title:"UCSD"
+ 	});
 }
